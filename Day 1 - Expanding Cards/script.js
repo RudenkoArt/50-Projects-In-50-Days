c0e7ea88d9a5bea9c11bb1fe988a2f7b42677895
RudenkoArt <rudnkoart@gmail.com>
2022-10-31 15:42:54 +0200

    Finished the 1st day project

--- Day 1 - Expanding Cards/script.js
+++ Day 1 - Expanding Cards/script.js
@@ -1,1 +1,14 @@
-console.log('lol');
+const panels = document.querySelectorAll('.panel');
+
+function removeAcviteClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
+
+panels.forEach((panel) =>
+  panel.addEventListener('click', () => {
+    removeAcviteClasses();
+    panel.classList.add('active');
+  })
+);
